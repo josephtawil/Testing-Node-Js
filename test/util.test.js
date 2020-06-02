5f6ffffe85d7f77bde64a2869186360102086ce8
Joseph Tawil <jtawil2015@gmail.com>
2020-06-01 19:51:21 -0700

    added add and minus test functions

--- test/util.test.js
+++ test/util.test.js
@@ -1,4 +1,5 @@
-const { add } = require("../util/util");
+//using test to have less errors when writing code and to develop code faster
+const { add, minus } = require("../util/util");
 
 describe("Add", () => {
 
@@ -11,9 +12,33 @@ describe("Add", () => {
 
     describe("Failure", () => {
         //toBe is the result we expect
-        it("Should throw an error if 1st argument isn't a string", () => {
+        it("Should throw an error if 1st argument isn't a number", () => {
             expect(() => add("2", 2)).toThrowError();
+        });
+        it("Should throw an error if 2nd argument isn't a number", () => {
+            expect(() => add(2, "2")).toThrowError();
+        });
+        it("Should throw an error if no arguments were received", () => {
+            expect(() => add()).toThrowError();
         });
     });
 });
 
+describe("Minus", () => {
+    describe("Success", () => {
+        it("Should return the subtraction of 2 past arguments", () => {
+            expect(minus(4, 2)).toBe(2);
+        });
+    });
+    describe("Failure", () => {
+        it("Should throw an argument if 1st argument is not a number", () => {
+            expect(() => minus("2", 2)).toThrowError();
+        });
+        it("Should throw an argument if 2nd argument is not a number", () => {
+            expect(() => minus(2, "2")).toThrowError();
+        });
+        it("Should throw an error if no arguments were received", () => {
+            expect(() => minus()).toThrowError();
+        });
+    });
+});
--- util/util.js
+++ util/util.js
@@ -1,6 +1,12 @@
 const add = (num1, num2) => {
     if (typeof num1 != "number") throw new Error("First argument must be a number");
+    if (typeof num2 != "number") throw new Error("Second argument must be a number");
     return num1 + num2;
 };
 
-module.exports = { add };
+const minus = (num1, num2) => {
+    if (typeof num1 != "number") throw new Error("First argument must be a number");
+    if (typeof num2 != "number") throw new Error("Second argument must be a number");
+    return num1 - num2;
+}
+module.exports = { add, minus };
