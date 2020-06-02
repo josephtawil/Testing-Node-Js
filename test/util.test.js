30706fb821db0cf5776e7a20ef2e11f01bb370cf
Joseph Tawil <jtawil2015@gmail.com>
2020-06-01 20:44:07 -0700

    added multiply, divide, and power test functions using jest

--- test/util.test.js
+++ test/util.test.js
@@ -1,5 +1,5 @@
 //using test to have less errors when writing code and to develop code faster
-const { add, minus } = require("../util/util");
+const { add, minus, multiply, divide, power } = require("../util/util");
 
 describe("Add", () => {
 
@@ -39,6 +39,64 @@ describe("Minus", () => {
         });
         it("Should throw an error if no arguments were received", () => {
             expect(() => minus()).toThrowError();
+        });
+    });
+});
+
+describe("Multiply", () => {
+    describe("Success", () => {
+        it("Should multiply the two arguments given", () => {
+            expect(multiply(2, 2)).toBe(4);
+        });
+    });
+    describe("Failure", () => {
+        it("Should throw an argument if 1st argument is not a number", () => {
+            expect(() => multiply("2", 2)).toThrowError();
+        });
+        it("Should throw an argument if 2nd argument is not a number", () => {
+            expect(() => multiply(2, "2")).toThrowError();
+        });
+        it("Should throw an error if no arguments were received", () => {
+            expect(() => multiply()).toThrowError();
+        });
+    });
+});
+describe("Divide", () => {
+    describe("Success", () => {
+        it("Should multiply the two arguments given", () => {
+            expect(divide(4, 2)).toBe(2);
+        });
+    });
+    describe("Failure", () => {
+        it("Should throw an error if 1st argument is not a number", () => {
+            expect(() => divide("2", 2)).toThrowError();
+        });
+        it("Should throw an error if 2nd argument is not a number", () => {
+            expect(() => divide(2, "2")).toThrowError();
+        });
+        it("Should throw an error if 2nd argument is a 0", () => {
+            expect(() => divide(2, 0)).toThrowError();
+        });
+        it("Should throw an error if no arguments were received", () => {
+            expect(() => divide()).toThrowError();
+        });
+    });
+});
+describe("Power", () => {
+    describe("Success", () => {
+        it("Should multiply the two arguments given", () => {
+            expect(power(2, 2)).toBe(4);
+        });
+    });
+    describe("Failure", () => {
+        it("Should throw an argument if 1st argument is not a number", () => {
+            expect(() => power("2", 2)).toThrowError();
+        });
+        it("Should throw an argument if 2nd argument is not a number", () => {
+            expect(() => power(2, "2")).toThrowError();
+        });
+        it("Should throw an error if no arguments were received", () => {
+            expect(() => power()).toThrowError();
         });
     });
 });
--- util/util.js
+++ util/util.js
@@ -9,4 +9,22 @@ const minus = (num1, num2) => {
     if (typeof num2 != "number") throw new Error("Second argument must be a number");
     return num1 - num2;
 }
-module.exports = { add, minus };
+
+const multiply = (num1, num2) => {
+    if (typeof num1 != "number") throw new Error("First argument must be a number");
+    if (typeof num2 != "number") throw new Error("Second argument must be a number");
+    return num1 * num2;
+};
+
+const divide = (num1, num2) => {
+    if (typeof num1 != "number") throw new Error("First argument must be a number");
+    if (typeof num2 != "number") throw new Error("Second argument must be a number");
+    if (num2 === 0) throw new Error("Second argument must not be 0, undefined");
+    return num1 / num2;
+};
+const power = (num1, num2) => {
+    if (typeof num1 != "number") throw new Error("First argument must be a number");
+    if (typeof num2 != "number") throw new Error("Second argument must be a number");
+    return Math.pow(num1, num2);
+};
+module.exports = { add, minus, multiply, divide, power };
